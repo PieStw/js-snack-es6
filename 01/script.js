864bac03a938866d41bb0ee14724fe842c5ded02
PieStw <piero.stalteri@icloud.com>
2024-10-25 14:31:58 +0200

    esercizio 1

---
+++ 01/script.js
@@ -0,0 +1,29 @@
+//Creare un array di oggetti:
+//Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
+//Stampare a schermo la bici con peso minore.
+
+
+
+
+let lista = []
+
+for(let i = 0; i < 10; i++){
+
+    const bici = {nome:`bici: ${i}`, peso: Math.floor(Math.random() * 100 + 1)};
+    lista.push(bici);
+
+}
+
+let minPeso = 1000;
+let posizione = 0;
+
+
+lista.forEach((element, index) => {
+    if(element.peso <= minPeso){
+        minPeso = element.peso;
+        posizione = index;
+    }
+});
+
+console.log(lista);
+console.log(`La bici più leggera è ${lista[posizione].nome} con un peso di ${minPeso}`);
